a6af41ab33990caf5e00bf100d6406850d71b196
utstb <sebastian@bunda.nl>
2025-08-12 14:15:26 +0200

    add init data class

--- data/__init__.py
+++ data/__init__.py
@@ -8,3 +8,17 @@ from .pytorch_dataset import MNIST, ImageNet, CIFAR10, CIFAR100, SVHN
 __all__ = [
     'MNIST', 'ImageNet', 'CIFAR10', 'CIFAR100', 'SVHN',
 ]
+
+DATASET_CLASSES = {
+    'mnist': MNIST,
+    'cifar10': CIFAR10,
+    'cifar100': CIFAR100,
+    'svhn': SVHN,
+    'imagenet': ImageNet,
+}
+
+def get_dataset_class(name: str):
+    try:
+        return DATASET_CLASSES[name.lower()]
+    except KeyError:
+        raise ValueError(f"Unknown dataset: {name}. Available options: {list(DATASET_CLASSES.keys())}")
